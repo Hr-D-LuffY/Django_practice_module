ec3056ae9f6a9b3a15d036d9a9137a436e26cc8d
Hr-D-LuffY <hrsiam420@gmail.com>
2024-01-08 00:27:13 +0600

    Module 22/5 & 23.5 Added

---
+++ Module23_5/mamar_bank/transactions/constants.py
@@ -0,0 +1,16 @@
+DEPOSIT = 1
+WITHDRAWAL = 2
+LOAN = 3
+LOAN_PAID = 4
+SEND_MONEY =5
+RECIEVE_MONEY =6
+
+TRANSACTION_TYPE = (
+    (DEPOSIT, 'Deposite'),
+    (WITHDRAWAL, 'Withdrawal'),
+    (LOAN, 'Loan'),
+    (LOAN_PAID, 'Loan Paid'),
+    (SEND_MONEY, 'Send Money'),
+    (RECIEVE_MONEY, 'Recieve Money'),
+    
+)
